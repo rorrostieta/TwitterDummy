bf1b04406b55e8c0da26af7494dc2a28d790daa6
Ruben Orrostieta <ruben.orrostieta@gmail.com>
2022-04-22 17:27:49 -0500

    update: typo correction adding missing ')'

--- test/services/userservice.test.js
+++ test/services/userservice.test.js
@@ -13,9 +13,9 @@ describe('Test for userservice', () =>{
   test('2) Get all user data in a list', () =>{
     const user = userservice.create(100, 'rorrostieta', 'ruben')
     const userInfoList = userservice.getInfo(user)
-    expect(userInfoList[0].toBe(1)
-    expect(userInfoList[1].toBe('rorrostieta')
-    expect(userInfoList[2].toBe('ruben')
-    expect(userInfoList[3].toBe('Sin Bio')
+    expect(userInfoList[0]).toBe(100)
+    expect(userInfoList[1]).toBe('rorrostieta')
+    expect(userInfoList[2]).toBe('ruben')
+    expect(userInfoList[3]).toBe('Sin Bio')
   })
 })
