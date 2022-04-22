72f41c4662edb0edf081c2d56213ee0461a6d9db
Ruben Orrostieta <ruben.orrostieta@gmail.com>
2022-04-22 17:12:58 -0500

    update: modificacion de typo en prueba

--- test/services/userservice.test.js
+++ test/services/userservice.test.js
@@ -3,7 +3,7 @@ const userservice = require('./../../app/services/userservice');
 describe('Test for UserService', () =>{
 
   test('1) Create new user using the UserService', () =>{
-    const user = UserService.create(100, 'rorrostieta', 'ruben')
+    const user = userservice.create(100, 'rorrostieta', 'ruben')
     expect(user.username).toBe('rorrostieta')
     expect(user.name).toBe('ruben')
     expect(user.id).toBe(100)
