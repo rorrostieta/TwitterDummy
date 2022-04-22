47dcfc81a14f8fbe7b1f881e04d9fd0f3cc26e44
Ruben Orrostieta <ruben.orrostieta@gmail.com>
2022-04-22 17:07:39 -0500

    add: creation of userservice.test

---
+++ test/services/userservice.test.js
@@ -0,0 +1,12 @@
+const userservice = require('./../../app/services/userservice');
+
+describe('Test for UserService', () =>{
+
+  test('1) Create new user using the UserService', () =>{
+    const user = UserService.create(100, 'rorrostieta', 'ruben')
+    expect(user.username).toBe('rorrostieta')
+    expect(user.name).toBe('ruben')
+    expect(user.id).toBe(100)
+    expect(user.bio).not.toBeUndefined()
+  })
+})
