f4f062b69a12a17ecd27f5a11480f96eeeeeedb1
Ruben Orrostieta <ruben.orrostieta@gmail.com>
2022-04-22 17:28:15 -0500

    add: adding the getInfo module to the userservice

--- app/services/userservice.js
+++ app/services/userservice.js
@@ -4,6 +4,10 @@ class userservice {
   static create(id, username, name) {
     return new user (id, username, name, 'Sin Bio')
   }
+
+  static getInfo(user){
+    return [user.id, user.username, user.name, user.bio]
+  }
 }
 
 module.exports = userservice
