7787ec851981317ca4e503fc41d4175c14dfaad0
Ruben Orrostieta <ruben.orrostieta@gmail.com>
2022-04-21 23:48:06 -0500

    add: module with verified by test for user creation

---
+++ app/models/user.js
@@ -0,0 +1,12 @@
+class user {
+  constructor(id, username, name, bio, dateCreated, lastUpdate) {
+    this.id = id
+    this.username = username
+    this.name = name
+    this.bio = bio
+    this.dateCreated = dateCreated
+    this.lastUpdate = lastUpdate
+  }
+}
+
+module.exports = user
